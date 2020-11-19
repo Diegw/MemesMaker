b0e50870c036dd087a1af298749f44c492265f9f
Diego Wechselberg <diego.wechselberg@gmail.com>
2020-11-19 20:44:02 -0300

    add playerinput on click, go to menu scene

---
+++ Assets/Scripts/InputReceptor.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputReceptor : MonoBehaviour
+{
+    public static Action OnAcceptEvent;
+    public static Action OnDeclineEvent;
+    public static Action OnAnyEvent;
+
+    private PlayerInputManager _playerInputManager = null;
+    private Controls _controls = null;
+    private PlayerInput _playerInput = null;
+
+    private InputAction _directionAction = null;
+    private InputAction _acceptAction = null;
+    private InputAction _declineAction = null;
+    private InputAction _anyAction = null;
+
+    private const string DIRECTION = "Direction";
+    private const string ACCEPT = "Accept";
+    private const string DECLINE = "Decline";
+    private const string ANY = "Any";
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        _playerInputManager = FindObjectOfType<PlayerInputManager>();
+        if(_playerInputManager == null)
+        {
+            Debug.LogError("Couldnt find PlayerInputManager in Scene");
+            return;
+        }
+        transform.SetParent(_playerInputManager.transform);
+
+        _playerInput = GetComponent<PlayerInput>();
+        if(_playerInput == null)
+        {
+            Debug.LogError("Couldnt find PlayerInput Component in PlayerInput Prefab");
+            return;
+        }
+
+        _controls = new Controls();
+    }
+
+    private void Start()
+    {
+        _playerInput.currentActionMap = _controls.Player;
+        _playerInput.actions.Enable();
+
+        _directionAction = _playerInput.actions[DIRECTION];
+        _acceptAction = _playerInput.actions[ACCEPT];
+        _declineAction = _playerInput.actions[DECLINE];
+        _anyAction = _playerInput.actions[ANY];
+
+        SubscribeToPlayerInput();
+    }
+
+    private void SubscribeToPlayerInput()
+    {
+        if (_playerInput == null)
+        {
+            return;
+        }
+
+        _acceptAction.started += AcceptEvent;
+        _declineAction.started += DeclineEvent;
+        _anyAction.started += AnyEvent;
+    }
+
+    private void UnsubscribeToPlayerInput()
+    {
+        if (_playerInput == null)
+        {
+            return;
+        }
+
+        _acceptAction.started += AcceptEvent;
+        _declineAction.started += DeclineEvent;
+        _anyAction.started += AnyEvent;
+    }
+
+    private void AcceptEvent(InputAction.CallbackContext context)
+    {
+        OnAcceptEvent?.Invoke();
+    }
+
+    private void DeclineEvent(InputAction.CallbackContext context)
+    {
+        OnDeclineEvent?.Invoke();
+    }
+
+    private void AnyEvent(InputAction.CallbackContext context)
+    {
+        OnAnyEvent?.Invoke();
+    }
+
+    public Vector2 GetDirection()
+    {
+        return _directionAction.ReadValue<Vector2>();
+    }
+
+    private void OnDestroy()
+    {
+        UnsubscribeToPlayerInput();
+    }
+}
---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance => _instance;
+    public static Action OnSceneChanged;
+
+    private static ScenesManager _instance = null;
+    private string _currentSceneName = string.Empty;
+
+    private void Awake()
+    {
+        if(_instance != null)
+        {
+            Destroy(_instance);
+        }
+        _instance = this;
+        DontDestroyOnLoad(this);
+
+        SetCurrentScene(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SetCurrentScene;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SetCurrentScene;
+    }
+
+    private void SetCurrentScene(Scene scene, LoadSceneMode sceneMode)
+    {
+        _currentSceneName = scene.name;
+        OnSceneChanged?.Invoke();
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    
+    private void OnDestroy()
+    {
+        OnDisable();
+    }
+}
