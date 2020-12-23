d9b4a8f426e132aca246aecf6817930c391811e2
Diego Wechselberg <diego.wechselberg@gmail.com>
2020-12-23 17:27:27 -0300

    connect to server

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,33 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public static Action OnLocalSelectedEvent;
+    public static Action OnOnlineSelectedEvent;
+    public static Action<bool> OnConectivitySelectedEvent;
+    
     [SerializeField] private Button _singlePlayerButton = null;
     [SerializeField] private Button _multiPlayerButton = null;
     private Button[] buttons = null;
 
     private void Awake()
     {
-        ButtonsAddListener();
         buttons = FindObjectsOfType<Button>();
         ObjectSelect.Instance.SelectObject(_singlePlayerButton.gameObject);
     }
 
+    private void OnEnable()
+    {
+        ButtonsAddListener();
+    }
+
+    private void OnDisable()
+    {
+        ButtonsRemoveListener();
+    }
+
     private void ButtonsAddListener()
     {
-        _singlePlayerButton.onClick.AddListener(SinglePlayerButton);
-        _multiPlayerButton.onClick.AddListener(MultiPlayerButton);
+        _singlePlayerButton.onClick.AddListener(LocalButton);
+        _multiPlayerButton.onClick.AddListener(OnlineButton);
     }
 
-    public void SinglePlayerButton()
+    private void ButtonsRemoveListener()
     {
-        DisableButtons();
+        _singlePlayerButton.onClick.RemoveListener(LocalButton);
+        _multiPlayerButton.onClick.RemoveListener(OnlineButton);
     }
 
-    private void MultiPlayerButton()
+    public void LocalButton()
     {
         DisableButtons();
+        OnConectivitySelectedEvent?.Invoke(true);
+        OnLocalSelectedEvent?.Invoke();
+    }
+
+    private void OnlineButton()
+    {
+        DisableButtons();
+        OnConectivitySelectedEvent?.Invoke(false);
+        OnOnlineSelectedEvent?.Invoke();
     }
 
     private void DisableButtons()
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static Action OnConectedToServerEvent;
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        Menu.OnConectivitySelectedEvent += Connect;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        Menu.OnConectivitySelectedEvent -= Connect;
+    }
+
+    public void Connect(bool conectivityState)
+    {
+        PhotonNetwork.GameVersion = "0.1";
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.MinimalTimeScaleToDispatchInFixedUpdate = 0f;
+        PhotonNetwork.ConnectUsingSettings(PhotonNetwork.PhotonServerSettings.AppSettings, conectivityState);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        OnConectedToServerEvent?.Invoke();
+    }
+}
