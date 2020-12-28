9c709a79f1ec0905d660591e43cb024f964f83ae
Diego Wechselberg <diego.wechselberg@gmail.com>
2020-12-28 00:03:12 -0300

    add buttons funtionality for menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,18 +4,18 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public static Action OnLocalSelectedEvent;
-    public static Action OnOnlineSelectedEvent;
+    // public static Action OnLocalSelectedEvent;
+    // public static Action OnOnlineSelectedEvent;
     public static Action<bool> OnConectivitySelectedEvent;
     
-    [SerializeField] private Button _singlePlayerButton = null;
-    [SerializeField] private Button _multiPlayerButton = null;
+    [SerializeField] private Button _localButton = null;
+    [SerializeField] private Button _onlineButton = null;
     private Button[] buttons = null;
 
     private void Awake()
     {
-        buttons = FindObjectsOfType<Button>();
-        ObjectSelect.Instance.SelectObject(_singlePlayerButton.gameObject);
+        buttons = new Button[2] {_localButton, _onlineButton};
+        ObjectSelect.Instance.SelectObject(_localButton.gameObject);
     }
 
     private void OnEnable()
@@ -30,28 +30,28 @@ public class Menu : MonoBehaviour
 
     private void ButtonsAddListener()
     {
-        _singlePlayerButton.onClick.AddListener(LocalButton);
-        _multiPlayerButton.onClick.AddListener(OnlineButton);
+        _localButton.onClick.AddListener(LocalButton);
+        _onlineButton.onClick.AddListener(OnlineButton);
     }
 
     private void ButtonsRemoveListener()
     {
-        _singlePlayerButton.onClick.RemoveListener(LocalButton);
-        _multiPlayerButton.onClick.RemoveListener(OnlineButton);
+        _localButton.onClick.RemoveListener(LocalButton);
+        _onlineButton.onClick.RemoveListener(OnlineButton);
     }
 
     public void LocalButton()
     {
         DisableButtons();
         OnConectivitySelectedEvent?.Invoke(true);
-        OnLocalSelectedEvent?.Invoke();
+        // OnLocalSelectedEvent?.Invoke();
     }
 
     private void OnlineButton()
     {
         DisableButtons();
         OnConectivitySelectedEvent?.Invoke(false);
-        OnOnlineSelectedEvent?.Invoke();
+        // OnOnlineSelectedEvent?.Invoke();
     }
 
     private void DisableButtons()
